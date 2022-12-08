16804ffd4a5a2ab77f72fdfa03c7049b59b359d2
Ajibola Salami <ayo.lolade@gmail.com>
2022-12-08 16:01:02 +0100

    update

--- src/LaravelControllerTrait.php
+++ src/LaravelControllerTrait.php
@@ -3,7 +3,7 @@ namespace Lolaji\LaravelControllerTrait;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
-use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Support\Facades\Validator;
 
 
 trait LaravelControllerTrait 
